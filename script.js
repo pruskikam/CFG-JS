36a68a0cb01004f84aa975af4aebaa51fd5bcdd3
pruskikam <123952709+pruskikam@users.noreply.github.com>
2023-09-07 18:43:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+let button;
+let person = prompt('Please, enter your name!');
+let gallery;
+let paintings;
+let curtains;
+let score = 0;
+let count = 0;
+let finalScore;
+let h1;
+let h2;
+let input;
+let shadow;
+
+const paintingInfo = [
+	{ title: 'Self-portrait', surname: 'Rembrandt', year: 1659 },
+	{ title: 'The starry night', surname: 'Van gogh', year: 1889 },
+	{ title: 'The Birth of Venus', surname: 'Boticelli', year: 1485 },
+	{ title: 'Nighthawks', surname: 'Hopper', year: 1942 },
+	{ title: 'Mona Lisa', surname: 'da Vinci', year: 1485 },
+	{ title: 'Guernica', surname: 'Picasso', year: 1937 },
+	{ title: 'The Young Lady with Gloves', surname: 'Lempicka', year: 1930 },
+	{ title: 'American Gothic', surname: 'Wood', year: 1930 },
+];
+
+const main = () => {
+	prepareElements();
+	prepareEvents();
+	showName();
+};
+
+const prepareElements = () => {
+	button = document.querySelector('button');
+	alert = document.querySelector('.alert-info');
+	h2 = document.querySelector('h2');
+	gallery = document.getElementById('gallery');
+	paintings = document.getElementsByClassName('painting');
+	curtains = document.getElementsByClassName('curtain');
+	input = document.querySelector('input');
+	popup = document.getElementById('popup');
+	finalScore = document.getElementById('final-score');
+	intro = document.querySelector('.intro');
+	shadow = document.getElementsByClassName('shadow');
+};
+
+const prepareEvents = () => {
+	button.addEventListener('click', startQuiz);
+	input.addEventListener('keyup', checkEnter);
+};
+
+const showName = () => {
+	h2.innerText = `Hello ${person}`;
+};
+
+const startQuiz = () => {
+	paintings[count]
+		.getElementsByClassName('curtain')[0]
+		.classList.add('curtain-up');
+
+	if (input.value == !'') {
+		checkEnter();
+	}
+};
+
+function checkEnter(e) {
+	const isEnter = e.which == 13 || e.keyCode == 13;
+	if (isEnter) {
+		checkInput();
+	}
+}
+
+const checkInput = () => {
+	if (input.value !== '') {
+		checkTheAnswer();
+	}
+	if (input.value === '') {
+		//wyswietl wiadomosc!!!!!!!!!
+	}
+};
+
+const clear = () => {
+	input.value = '';
+};
+
+const checkTheAnswer = () => {
+	let answer = input.value.toLowerCase();
+	let surname = paintingInfo[count].surname.toString().toLowerCase();
+	if (surname === answer) {
+		count = count + 1;
+		score = score + 1;
+		setTimeout(clear, 1000);
+		if (count == 7) {
+			setTimeout(endQuiz, 1500);
+		}
+		startQuiz();
+	}
+
+	if (surname !== answer) {
+		count = count + 1;
+		setTimeout(clear, 1000);
+		if (count == 7) {
+			setTimeout(endQuiz, 1500);
+		}
+		startQuiz();
+	}
+};
+
+const endQuiz = () => {
+	intro.classList.add('hide');
+	h2.innerText = `your score is: ${score}/8`;
+
+	for (i = 0, all = shadow.length; i < all; i++) {
+		shadow[i].classList.add('show');
+		// setTimeout(rightSwipe, 1500);
+	}
+};
+
+
+const noAnswerAlert = () => {
+	alert.innerText = 'You need to put the surname first!';
+};
+
+document.addEventListener('DOMContentLoaded', main);
